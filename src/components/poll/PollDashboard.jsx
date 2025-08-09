ba3dc409ce534e3c9f5bfe1608260eeb29c2ea25
nemeryous <nammp.22it@vku.udn.vn>
2025-08-09 11:30:10 +0700

    update ui/poll

--- src/components/poll/PollDashboard.jsx
+++ src/components/poll/PollDashboard.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import PollItem from "./PollItem";
+import { pollsData } from "@utils/constants";
+import ParticipantList from "./ParticipantList/ParticipantList";
 
 const PollDashboard = () => {
   return (
@@ -12,6 +15,27 @@ const PollDashboard = () => {
             Đang diễn ra
           </span>
         </div>
+
+        {pollsData.map((poll) => (
+          <PollItem
+            key={poll.id}
+            pollQuestion={poll.pollQuestion}
+            pollType={poll.pollType}
+            pollResponsesNum={poll.pollResponsesNum}
+            pollOptions={poll.pollOptions}
+          />
+        ))}
+      </div>
+      <div className="animate-fadeInUP rounded-2xl bg-white p-[25px] shadow-[0_4px_20px_rgba(0,0,0,0.08)] transition duration-300 ease-in-out hover:-translate-y-[5px] hover:shadow-[0_8px_30px_rgba(0,0,0,0.12)]">
+        <div className="mb-5 flex items-center justify-between">
+          <h3 className="text-[1.3rem] font-semibold text-[#333]">
+            Danh sách người tham gia
+          </h3>
+          <span className="rounded-2xl bg-[#fbc02d] px-3 py-1 text-[0.8rem] font-medium text-[#333]">
+            287 người
+          </span>
+        </div>
+        <ParticipantList />
       </div>
     </div>
   );
--- src/pages/poll/PollAnalystic.jsx
+++ src/pages/poll/PollAnalystic.jsx
@@ -6,13 +6,13 @@ import React from "react";
 const PollAnalystic = () => {
   return (
     <div className="px-0 py-7">
-      <div className='"max-w-[1200px] mx-auto px-5'>
+      <div className="mx-auto max-w-[1200px] px-5">
         <h1 className="mb-8 text-center text-4xl font-bold text-[#e53935]">
           Thống kê bình chọn sự kiện
-          <EventBanner />
-          <StatisticsOverview />
-          <PollDashboard />
         </h1>
+        <EventBanner />
+        <StatisticsOverview />
+        <PollDashboard />
       </div>
     </div>
   );
